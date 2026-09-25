9f6563a76159448dbd7664c7224f59aaafe8ae51
Sascha Philipp <saschaphilipp28@gmail.com>
2026-09-25 22:36:53 +0200

    Add files via upload

---
+++ Onlineshop/Objekt-Liste-Array.py
@@ -0,0 +1,132 @@
+#Liste mit 10 Grafikkarten 
+
+grafikkarten = [
+    {
+        "id": 1,
+        "name": "RTX 3060",
+        "hersteller": "NVIDIA",
+        "preis": 329.99,
+        "speicher_gb": 12,
+        "anschluss": "PCIe 4.0",
+        "leistungsklasse": "Mittelklasse",
+        "stromverbrauch_watt": 170,
+        "kuehlung": "Luft",
+        "lagernd": True
+    },
+    {
+        "id": 2,
+        "name": "RTX 3070",
+        "hersteller": "NVIDIA",
+        "preis": 499.99,
+        "speicher_gb": 8,
+        "anschluss": "PCIe 4.0",
+        "leistungsklasse": "Oberklasse",
+        "stromverbrauch_watt": 220,
+        "kuehlung": "Luft",
+        "lagernd": True
+    },
+    {
+        "id": 3,
+        "name": "RX 6700 XT",
+        "hersteller": "AMD",
+        "preis": 379.99,
+        "speicher_gb": 12,
+        "anschluss": "PCIe 4.0",
+        "leistungsklasse": "Mittelklasse",
+        "stromverbrauch_watt": 230,
+        "kuehlung": "Luft",
+        "lagernd": True
+    },
+    {
+        "id": 4,
+        "name": "RX 6800",
+        "hersteller": "AMD",
+        "preis": 549.99,
+        "speicher_gb": 16,
+        "anschluss": "PCIe 4.0",
+        "leistungsklasse": "Oberklasse",
+        "stromverbrauch_watt": 250,
+        "kuehlung": "Luft",
+        "lagernd": True
+    },
+    {
+        "id": 5,
+        "name": "RTX 3080",
+        "hersteller": "NVIDIA",
+        "preis": 699.99,
+        "speicher_gb": 10,
+        "anschluss": "PCIe 4.0",
+        "leistungsklasse": "High-End",
+        "stromverbrauch_watt": 320,
+        "kuehlung": "Luft",
+        "lagernd": True
+    },
+    {
+        "id": 6,
+        "name": "RX 6600",
+        "hersteller": "AMD",
+        "preis": 249.99,
+        "speicher_gb": 8,
+        "anschluss": "PCIe 4.0",
+        "leistungsklasse": "Einsteiger",
+        "stromverbrauch_watt": 132,
+        "kuehlung": "Luft",
+        "lagernd": True
+    },
+    {
+        "id": 7,
+        "name": "RTX 3050",
+        "hersteller": "NVIDIA",
+        "preis": 279.99,
+        "speicher_gb": 8,
+        "anschluss": "PCIe 4.0",
+        "leistungsklasse": "Einsteiger",
+        "stromverbrauch_watt": 130,
+        "kuehlung": "Luft",
+        "lagernd": True
+    },
+    {
+        "id": 8,
+        "name": "RX 6950 XT",
+        "hersteller": "AMD",
+        "preis": 799.99,
+        "speicher_gb": 16,
+        "anschluss": "PCIe 4.0",
+        "leistungsklasse": "High-End",
+        "stromverbrauch_watt": 335,
+        "kuehlung": "Luft",
+        "lagernd": False
+    },
+    {
+        "id": 9,
+        "name": "RTX 3090",
+        "hersteller": "NVIDIA",
+        "preis": 1499.99,
+        "speicher_gb": 24,
+        "anschluss": "PCIe 4.0",
+        "leistungsklasse": "High-End",
+        "stromverbrauch_watt": 350,
+        "kuehlung": "Luft",
+        "lagernd": False
+    },
+    {
+        "id": 10,
+        "name": "GTX 1660",
+        "hersteller": "NVIDIA",
+        "preis": 219.99,
+        "speicher_gb": 6,
+        "anschluss": "PCIe 3.0",
+        "leistungsklasse": "Einsteiger",
+        "stromverbrauch_watt": 120,
+        "küehlung": "Luft",
+        "lagernd": False 
+    }
+
+]
+
+# Ganze Liste Ausgeben = print(grafikkarten)
+# Zugriff auf bestimmtes Objekt print = (grafikkarten [0] )
+# Zugriff auf einzelnes Feld = print(grafikkarte[4]["name")
+# for karte in grafikkarte ["lagernd": print(karte["name"],"ist auf Lager")]
+
+grafikkarte ["lagernd": print(karte["name"],"ist auf Lager")]
